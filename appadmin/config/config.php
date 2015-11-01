6b34b8b29777592b9797f549fbdfc3460571c3f1
yindashan <yindashan@meituan.com>
2015-11-01 13:37:05 +0800

    edit config.php cookie name

--- appadmin/config/config.php
+++ appadmin/config/config.php
@@ -243,7 +243,7 @@ $config['encryption_key'] = 'myanes&dskpw:soe';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
-$config['sess_cookie_name']		= 'ci_sessionm';
+$config['sess_cookie_name']		= 'ci_sessionh';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
