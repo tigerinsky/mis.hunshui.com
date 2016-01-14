f628c8370b5cccb52875b072d1729cb0953801b3
tigerinsky@foxmail.com <chenhao@god.org>
2016-01-14 14:13:06 +0800

    fix order config

--- appadmin/config/order.php
+++ appadmin/config/order.php
@@ -1,10 +1,9 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$host = 'http://www.hunwater.com';
 $config['request'] = array(
-    'advance_pay' => $host.'/order_api/plat_advance',
-    'plat_pay' => $host.'/order_api/plat_pay_record',
-    'cancel' => $host.'/order_api/cancel_order',
-    'refund' => $host.'/order_api/refund_order',
-    'modify' => $host.'/order_api/modify_price',
+    'advance_pay' => 'http://www.hunwater.com'.'/order_api/plat_advance',
+    'plat_pay' => 'http://www.hunwater.com'.'/order_api/plat_pay_record',
+    'cancel' => 'http://www.hunwater.com'.'/order_api/cancel_order',
+    'refund' => 'http://www.hunwater.com'.'/order_api/refund_order',
+    'modify' => 'http://www.hunwater.com'.'/order_api/modify_price',
 );
